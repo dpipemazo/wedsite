f08ee76f3b46309bd7e24302fbaa5bb92c83358f
Dan Pipe-Mazo <dan@kunasystems.com>
2017-08-26 21:15:41 -0700

    index: have most of the story section done

--- wedding/views.py
+++ wedding/views.py
@@ -7,12 +7,44 @@ from django.urls import reverse
 
 def index(request):
     context = {}
-    return render(request, 'wedding/index.html', context)
+    return render(request, 'wedding/pages/index.html', context)
 
 def details(request):
     context = {}
-    return render(request, 'wedding/details.html', context)
+    return render(request, 'wedding/pages/details.html', context)
 
 def contact(request):
     context = {}
-    return render(request, 'wedding/contact.html', context)
+    return render(request, 'wedding/pages/contact.html', context)
+
+def story(request):
+    context = {}
+    return render(request, 'wedding/pages/story.html', context)
+
+def wedding(request):
+    context = {}
+    return render(request, 'wedding/pages/wedding.html', context)
+
+def events(request):
+    context = {}
+    return render(request, 'wedding/pages/events.html', context)
+
+def travel(request):
+    context = {}
+    return render(request, 'wedding/pages/travel.html', context)
+
+def explore(request):
+    context = {}
+    return render(request, 'wedding/pages/explore.html', context)
+
+def gifts(request):
+    context = {}
+    return render(request, 'wedding/pages/gifts.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'wedding/pages/contact.html', context)
+
+def rsvp(request):
+    context = {}
+    return render(request, 'wedding/pages/rsvp.html', context)
