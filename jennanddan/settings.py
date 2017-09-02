7896ad92dc160e3a12fe7d26b3d7166ee9594931
Dan Pipe-Mazo <dan@kunasystems.com>
2017-09-02 16:41:38 -0700

    wedding: simplification, basic auth
    - Switched wedding over to base HTML template
    - Switched views over to class-based, cut down on copypasta
    - Added basic login/logout functionality.

--- jennanddan/settings.py
+++ jennanddan/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'wedding',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wedding',
     'easy_maps',
 ]
 
--- wedding/views.py
+++ wedding/views.py
@@ -2,49 +2,28 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.views import redirect_to_login
 
-def index(request):
-    context = {}
-    return render(request, 'wedding/pages/index.html', context)
+class StaticView(View):
+    """
+    Basic static view. Shows a template back
+    """
+    template = None
 
-def details(request):
-    context = {}
-    return render(request, 'wedding/pages/details.html', context)
+    def get(self, request):
+        return render(request, "wedding/pages/" + self.template, {})
 
-def contact(request):
-    context = {}
-    return render(request, 'wedding/pages/contact.html', context)
-
-def story(request):
-    context = {}
-    return render(request, 'wedding/pages/story.html', context)
-
-def wedding(request):
-    context = {}
-    return render(request, 'wedding/pages/wedding.html', context)
-
-def events(request):
-    context = {}
-    return render(request, 'wedding/pages/events.html', context)
-
-def travel(request):
-    context = {}
-    return render(request, 'wedding/pages/travel.html', context)
-
-def explore(request):
-    context = {}
-    return render(request, 'wedding/pages/explore.html', context)
-
-def gifts(request):
-    context = {}
-    return render(request, 'wedding/pages/gifts.html', context)
-
-def contact(request):
-    context = {}
-    return render(request, 'wedding/pages/contact.html', context)
-
-def rsvp(request):
-    context = {}
-    return render(request, 'wedding/pages/rsvp.html', context)
+class RSVPView(View):
+    """
+    RSVP View. Will redirect to login page if the user isn't authenticated,
+    otherwise will allow them to use the RSVP system
+    """
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, "wedding/pages/rsvp.html", {})
+        else:
+            return redirect_to_login("/rsvp")
