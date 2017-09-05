89400af87cfee4aacd0b920f8b08f146d9f5734e
Dan Pipe-Mazo <dan@kunasystems.com>
2017-09-04 20:12:11 -0700

    settings: update raven git path

--- jennanddan/settings.py
+++ jennanddan/settings.py
@@ -168,7 +168,7 @@ EMAIL_USE_TLS = True
 #
 RAVEN_CONFIG = {
     'dsn': os.environ.get('SENTRY_DSN'),
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': raven.fetch_git_sha(BASE_DIR),
 }
 
 # Timezone detection
