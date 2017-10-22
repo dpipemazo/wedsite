93f4d059d0e3be43aa1f0ea76eb16a62bdc7ae77
Dan Pipe-Mazo <dpipemazo@gmail.com>
2017-10-22 00:10:29 -0700

    wedding: change page permissions
    Story, Explore, Contact and Gifts pages no longer require sign-in

--- wedding/urls.py
+++ wedding/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from wedding.views import (
-    StaticView, RSVPView, CreateAccountView
+    StaticView, StaticViewNoAuth, RSVPView, CreateAccountView
 )
 from django.contrib.auth.views import (
     LoginView, PasswordResetView, PasswordChangeView, PasswordChangeDoneView,
@@ -12,15 +12,13 @@ urlpatterns = [
 
     # Top-level URL serves the main index view
     url(r'^$', StaticView.as_view(template="index.html"), name='index'),
-    url(r'^details$', StaticView.as_view(template="details.html"), name='details'),
-    url(r'^contact$', StaticView.as_view(template="contact.html"), name='contact'),
-    url(r'^story$', StaticView.as_view(template="story.html"), name='story'),
+    url(r'^contact$', StaticViewNoAuth.as_view(template="contact.html"), name='contact'),
+    url(r'^story$', StaticViewNoAuth.as_view(template="story.html"), name='story'),
     url(r'^wedding$', StaticView.as_view(template="wedding.html"), name='wedding'),
     url(r'^events$', StaticView.as_view(template="events.html"), name='events'),
     url(r'^travel$', StaticView.as_view(template="travel.html"), name='travel'),
-    url(r'^explore$', StaticView.as_view(template="explore.html"), name='explore'),
-    url(r'^gifts$', StaticView.as_view(template="gifts.html"), name='gifts'),
-    url(r'^contact$', StaticView.as_view(template="contact.html"), name='contact'),
+    url(r'^explore$', StaticViewNoAuth.as_view(template="explore.html"), name='explore'),
+    url(r'^gifts$', StaticViewNoAuth.as_view(template="gifts.html"), name='gifts'),
     url(r'^rsvp$', RSVPView.as_view(), name='rsvp'),
     url(r'^create-account$', CreateAccountView.as_view(), name='create-account'),
 ]
