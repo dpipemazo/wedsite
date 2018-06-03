76dfb629844a7ae11c60d5978685206da827f488
Dan Pipe-Mazo <dpipemazo@gmail.com>
2018-06-03 13:06:00 -0700

    django-wedsite: bump to 0.0.8

--- example_wedsite/settings.py
+++ example_wedsite/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'wedsite.context_processors.wedsite_context',
             ],
         },
     },
