cbb739a24c5aef73df5fdd2df7c9fb2031933cd0
Dan Pipe-Mazo <dpipemazo@gmail.com>
2018-06-03 10:04:20 -0700

    example_wedsite: updates for django-wedsite 0.0.5

--- example_wedsite/settings.py
+++ example_wedsite/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 import raven
 import wedsite
-from example_wedsite.wedsite_conf import CUSTOMIZED_JSON, CUSTOMIZED_ACCESS
+from example_wedsite.wedsite_conf import CUSTOMIZED_JSON
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -191,4 +191,3 @@ LOGIN_REDIRECT_URL = "/"
 
 # Make a new wedsite JSON object to test overriding things
 WEDSITE_JSON = CUSTOMIZED_JSON
-WEDSITE_ACCESS = CUSTOMIZED_ACCESS
